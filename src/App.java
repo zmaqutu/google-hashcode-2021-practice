9636a7609ab3a9a45cbe216af04e7178b4c8a4b0
Tswalano <moganegb@gmail.com>
2021-02-22 19:12:16 +0200

    file path update

--- src/App.java
+++ src/App.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -8,101 +7,143 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.ArrayList;
 import java.util.Arrays;
-
-import javax.swing.event.ListDataEvent;
+import java.util.ArrayList;
 
 public class App {
 
     static int TWO_PERSON_TEAM = 2;
     static int THREE_PERSON_TEAM = 3;
     static int FOUR_PERSON_TEAM = 4;
-    static String FILE_PATH = "C:\\Users\\mgngm\\Desktop\\AWS\\google-hashcode-2021-practice\\src\\data\\";
+    static String FOLDER = "\\src\\data\\";
+    static String FILE_PATH = System.getProperty("user.dir") + FOLDER;
+    // static String FILE_PATH = "C:\\Users\\mgngm\\Desktop\\AWS\\google-hashcode-2021-practice\\src\\data\\";
     static String FILE_NAME = "a_example";
 
     public static void main(String[] args) throws Exception {
 
-        // read();
+        AvailablePizza pizza = setDataFileVariables();
 
-        writeOutputResults();
+        deliverPizza(pizza);
 
     }
 
-    static void deliverPizza() {
+    static void deliverPizza(AvailablePizza pizza) {
         // TODO: Send out pizzas and minus from the available pizza
+        System.out.println(pizza);
+
+        int lvar = 0;
+
+        int M = pizza.getNumOfPizzaAvailable();
+        int T2 = (pizza.getTeamOf2() * TWO_PERSON_TEAM);
+        int T3 = (pizza.getTeamOf3() * THREE_PERSON_TEAM);
+        int T4 = (pizza.getTeamOf4() * FOUR_PERSON_TEAM);
+
+        var persons = List.of(pizza.getIngredients());
+
+        // List<Ingredients> byAge = person -> person.ge > 30;
+
+        List<Ingredients> ing = pizza.getIngredients();
+        // Stream<Ingredients> myStream = Arrays.stream(ing);
+
+        // TODO check if the ingredient already exists
+
+        // File output variables
+        int numberOfPizzaDelivered = 0; // Done
+        List<List> deliveredPizzas = new ArrayList<>();
+        List<String> pizzaIndex = new ArrayList<>();
+
+        // int total = T2 + T3 + T4;
+        // int idx = 0;
+
+        for (int i = 0; i < M; i++) {
+
+            // if (i != 0)
+            //     i--;
+
+            if (pizza.getTeamOf2() != 0) {
+                // Team - Piza Options
+
+                pizzaIndex.add(String.valueOf(ing.get(i).index));
+                pizzaIndex.add(String.valueOf(ing.get(i + 1).index));
+
+                deliveredPizzas.add(pizzaIndex);
+                numberOfPizzaDelivered += 1;
+                i += TWO_PERSON_TEAM;
+                pizza.setTeamOf2(pizza.getTeamOf2() - 1);
+
+            } else if (pizza.getTeamOf3() != 0) {
+
+                // Team - Piza Options
+                pizzaIndex.add(String.valueOf(ing.get(i).index));
+                pizzaIndex.add(String.valueOf(ing.get(i + 1).index));
+                pizzaIndex.add(String.valueOf(ing.get(i + 2).index)); 
+
+                deliveredPizzas.add(pizzaIndex);
+                // Default back
+                numberOfPizzaDelivered += 1;
+                pizza.setTeamOf3(pizza.getTeamOf3() - 1);
+                // i += THREE_PERSON_TEAM;
+            }
+        }
+
+        System.out.println(numberOfPizzaDelivered);
+
+        // TODO First calculate which teams will get a pizza
+        // TODO deliver ingredients while removing them from the list of ingredients
+        //
     }
 
-    static void read() {
+    static List<String> getContextOfFile() {
+
         Path filePath = Paths.get(FILE_PATH + FILE_NAME);
         Charset charset = StandardCharsets.UTF_8;
 
-        readDataFile(filePath, charset);
-
-    }
-
-    static AvailablePizza readDataFile(Path path, Charset charset) {
-
         try {
-            List<String> lines = Files.readAllLines(path, charset);
+            return Files.readAllLines(filePath, charset);
 
-            int availPizz = 0, T2 = 0, T3 = 0, T4 = 0;
-
-            String x = lines.get(0);
-            String[] nums = x.split(" ");
-
-            // for (int j = 0; j < nums.length; j++) {
-            // TODO Check for nulls
-            availPizz = Integer.parseInt(nums[0]);
-            T2 = Integer.parseInt(nums[1]);
-            T3 = Integer.parseInt(nums[2]);
-            T4 = Integer.parseInt(nums[3]);
-            // }
-
-            List<Ingredients> ingredients = new ArrayList<Ingredients>();
-
-            // TODO - BRB
-            for (int i = 1; i < lines.size(); i++) {
-
-                String[] rec = lines.get(i).split(" ");
-                String[] modRec = Arrays.copyOfRange(rec, 1, rec.length);
-
-                // Ingredients ing = new Ingredients(numberOfIngredients, ingredients)
-                // List<String> ingredients
-                Ingredients ing = new Ingredients(Integer.parseInt(rec[0]), Arrays.asList(modRec));
-                ingredients.add(ing);
-            }
-
-            // List<AvailablePizza> availablePizzas = new ArrayList<AvailablePizza>();
-            AvailablePizza availablePizza = new AvailablePizza(availPizz, T2, T3, T4, ingredients);
-            return availablePizza;
-
-        } catch (IOException ex) {
-            System.out.format("I/O error: %s%n", ex);
-            ex.printStackTrace();
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.format("I/O error: %s%n", e);
+            e.printStackTrace();
             return null;
         }
 
-        // TODO Count the number of teams that can add up to M number
+    }
 
-        // for (int i = 1; i < list.size()int; i++) {
+    static AvailablePizza setDataFileVariables() {
 
-        // System.out.println(list.get(i));
+        List<String> lines = getContextOfFile();
 
+        int availPizz, T2, T3, T4 = 0;
+
+        String x = lines.get(0);
+        String[] nums = x.split(" ");
+
+        // for (int j = 0; j < nums.length; j++) {
+        // TODO Check for nulls
+        availPizz = Integer.parseInt(nums[0]);
+        T2 = Integer.parseInt(nums[1]);
+        T3 = Integer.parseInt(nums[2]);
+        T4 = Integer.parseInt(nums[3]);
         // }
 
-        // String pizzaAndTeams = list.get(0).toString();
-        // String[] nums = pizzaAndTeams.split(" ");
+        List<Ingredients> ingredients = new ArrayList<Ingredients>();
 
-        // BaseNumbers baseNumbers = new BaseNumbers(nums[0], nums[1], nums[2],
-        // nums[3]);
+        // TODO - BRB
+        for (int i = 1; i < lines.size(); i++) {
 
-        // int M = Integer.parseInt(baseNumbers.getNumOfPizzaAvailable());
+            String[] rec = lines.get(i).split(" ");
+            String[] modRec = Arrays.copyOfRange(rec, 1, rec.length);
 
-        // int T2 = Integer.parseInt(baseNumbers.getTeamOf2()) * TWO_PERSON_TEAM;
-        // int T3 = Integer.parseInt(baseNumbers.getTeamOf3()) * THREE_PERSON_TEAM;
-        // int T4 = Integer.parseInt(baseNumbers.getTeamOf4()) * FOUR_PERSON_TEAM;
-        // return null;
+            Ingredients ing = new Ingredients((i - 1), Integer.parseInt(rec[0]), Arrays.asList(modRec));
+            ingredients.add(ing);
+        }
+
+        // List<AvailablePizza> availablePizzas = new ArrayList<AvailablePizza>();
+        AvailablePizza availablePizza = new AvailablePizza(availPizz, T2, T3, T4, ingredients);
+        return availablePizza;
+
     }
 
     static void writeOutputResults() {
@@ -177,10 +218,15 @@ public class App {
     }
 
     static class Ingredients {
+        int index;
         int numberOfIngredients;
         List<String> ingredients;
 
         // Getters
+        public int getIndex() {
+            return index;
+        }
+
         public int getNumberOfIngredients() {
             return numberOfIngredients;
         }
@@ -190,6 +236,10 @@ public class App {
         }
 
         // Setters
+        public void setIndex(int index) {
+            this.index = index;
+        }
+
         public void setNumberOfIngredients(int numberOfIngredients) {
             this.numberOfIngredients = numberOfIngredients;
         }
@@ -198,7 +248,8 @@ public class App {
             this.ingredients = ingredients;
         }
 
-        public Ingredients(int numberOfIngredients, List<String> ingredients) {
+        public Ingredients(int index, int numberOfIngredients, List<String> ingredients) {
+            this.index = index;
             this.ingredients = ingredients;
             this.numberOfIngredients = numberOfIngredients;
         }
