d4b6acbf8820df1f3ad615f8a74e1c2209f66a76
Tswalano <moganegb@gmail.com>
2021-02-24 01:39:55 +0200

    Can read data and output to a file

--- src/App.java
+++ src/App.java
@@ -5,9 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.ArrayList;
 
 public class App {
@@ -16,11 +15,21 @@ public class App {
     static int THREE_PERSON_TEAM = 3;
     static int FOUR_PERSON_TEAM = 4;
     static String FOLDER = "\\src\\data\\";
-    static String FILE_PATH = System.getProperty("user.dir") + FOLDER;
-    // static String FILE_PATH = "C:\\Users\\mgngm\\Desktop\\AWS\\google-hashcode-2021-practice\\src\\data\\";
+    // static String FILE_PATH = System.getProperty("user.dir") + FOLDER;
+    static String FILE_PATH = "C:\\Users\\mgngm\\Desktop\\AWS\\google-hashcode-2021-practice\\src\\data\\";
     static String FILE_NAME = "a_example";
 
     public static void main(String[] args) throws Exception {
+        // System.out.println(FILE_PATH);
+
+        // List<String> test = new ArrayList<String>();
+        // test.add("one");
+        // test.add("two");
+        // test.add("three");
+
+        // String joined2 = String.join(" ", test );
+
+        // System.out.println(joined2);
 
         AvailablePizza pizza = setDataFileVariables();
 
@@ -30,68 +39,92 @@ public class App {
 
     static void deliverPizza(AvailablePizza pizza) {
         // TODO: Send out pizzas and minus from the available pizza
-        System.out.println(pizza);
-
-        int lvar = 0;
 
         int M = pizza.getNumOfPizzaAvailable();
         int T2 = (pizza.getTeamOf2() * TWO_PERSON_TEAM);
         int T3 = (pizza.getTeamOf3() * THREE_PERSON_TEAM);
         int T4 = (pizza.getTeamOf4() * FOUR_PERSON_TEAM);
-
         var persons = List.of(pizza.getIngredients());
 
-        // List<Ingredients> byAge = person -> person.ge > 30;
-
         List<Ingredients> ing = pizza.getIngredients();
-        // Stream<Ingredients> myStream = Arrays.stream(ing);
 
         // TODO check if the ingredient already exists
 
         // File output variables
         int numberOfPizzaDelivered = 0; // Done
-        List<List> deliveredPizzas = new ArrayList<>();
-        List<String> pizzaIndex = new ArrayList<>();
+        // List<String> deliveredPizzas = new ArrayList<>();
+        List<String> deliveredPizzas = new ArrayList<String>();
+        List<Integer> teamOf = new ArrayList<>();
 
-        // int total = T2 + T3 + T4;
-        // int idx = 0;
-
+        // Send the deliveries
         for (int i = 0; i < M; i++) {
 
-            // if (i != 0)
-            //     i--;
+            // Not sure what this is ???
+            // TODO - NOT SURE - This function reduce the i with -1 to keep it within the
+            // range
+            if (i != 0)
+                i--;
+
+            // Clear the temporary list
+            List<String> pizzaIndex = new ArrayList<>();
 
             if (pizza.getTeamOf2() != 0) {
-                // Team - Piza Options
+                // Send it to a team of 2
+                for (int j = 0; j < TWO_PERSON_TEAM; j++) {
+                    if (pizzaIndex.size() > 0) {
+                        pizzaIndex.set(0, String.valueOf(TWO_PERSON_TEAM));
+                    } else {
+                        pizzaIndex.add(0, String.valueOf(TWO_PERSON_TEAM));
+                    }
+                    pizzaIndex.add(String.valueOf(ing.get(i + j).index));
+                }
 
-                pizzaIndex.add(String.valueOf(ing.get(i).index));
-                pizzaIndex.add(String.valueOf(ing.get(i + 1).index));
-
-                deliveredPizzas.add(pizzaIndex);
+                teamOf.add(TWO_PERSON_TEAM);
+                deliveredPizzas.add(String.join(" ", pizzaIndex));
                 numberOfPizzaDelivered += 1;
                 i += TWO_PERSON_TEAM;
                 pizza.setTeamOf2(pizza.getTeamOf2() - 1);
 
             } else if (pizza.getTeamOf3() != 0) {
+                // Send it to the team of 3
+                for (int j = 0; j < THREE_PERSON_TEAM; j++) {
+                    if (pizzaIndex.size() > 0) {
+                        pizzaIndex.set(0, String.valueOf(THREE_PERSON_TEAM));
+                    } else {
+                        pizzaIndex.add(0, String.valueOf(THREE_PERSON_TEAM));
+                    }
+                    pizzaIndex.add(String.valueOf(ing.get(i + j).index));
+                }
 
-                // Team - Piza Options
-                pizzaIndex.add(String.valueOf(ing.get(i).index));
-                pizzaIndex.add(String.valueOf(ing.get(i + 1).index));
-                pizzaIndex.add(String.valueOf(ing.get(i + 2).index)); 
-
-                deliveredPizzas.add(pizzaIndex);
-                // Default back
+                deliveredPizzas.add(String.join(" ", pizzaIndex));
                 numberOfPizzaDelivered += 1;
                 pizza.setTeamOf3(pizza.getTeamOf3() - 1);
-                // i += THREE_PERSON_TEAM;
+                i += THREE_PERSON_TEAM;
+
+            } else if (pizza.getTeamOf4() != 0) {
+                // Send it to the team of 4
+                for (int j = 0; j < FOUR_PERSON_TEAM; j++) {
+                    if (pizzaIndex.size() > 0) {
+                        pizzaIndex.set(0, String.valueOf(FOUR_PERSON_TEAM));
+                    } else {
+                        pizzaIndex.add(0, String.valueOf(FOUR_PERSON_TEAM));
+                    }
+                    pizzaIndex.add(String.valueOf(ing.get(i + j).index));
+                }
+
+                deliveredPizzas.add(String.join(" ", pizzaIndex));
+                numberOfPizzaDelivered += 1;
+                pizza.setTeamOf4(pizza.getTeamOf4() - 1);
+                i += FOUR_PERSON_TEAM;
             }
         }
 
-        System.out.println(numberOfPizzaDelivered);
+        writeOutputResults(numberOfPizzaDelivered, deliveredPizzas);
 
         // TODO First calculate which teams will get a pizza
         // TODO deliver ingredients while removing them from the list of ingredients
-        //
+        // TODO Pass the data to the output function
+        // TODO Create a function that will deliver unique pizzas
     }
 
     static List<String> getContextOfFile() {
@@ -120,13 +153,11 @@ public class App {
         String x = lines.get(0);
         String[] nums = x.split(" ");
 
-        // for (int j = 0; j < nums.length; j++) {
         // TODO Check for nulls
         availPizz = Integer.parseInt(nums[0]);
         T2 = Integer.parseInt(nums[1]);
         T3 = Integer.parseInt(nums[2]);
         T4 = Integer.parseInt(nums[3]);
-        // }
 
         List<Ingredients> ingredients = new ArrayList<Ingredients>();
 
@@ -140,16 +171,22 @@ public class App {
             ingredients.add(ing);
         }
 
-        // List<AvailablePizza> availablePizzas = new ArrayList<AvailablePizza>();
         AvailablePizza availablePizza = new AvailablePizza(availPizz, T2, T3, T4, ingredients);
         return availablePizza;
 
     }
 
-    static void writeOutputResults() {
+    static void writeOutputResults(int numberOfPizzaDelivered, List delList) {
+        System.out.format("Number of Pizza Delivered: %s%n", numberOfPizzaDelivered);
+        // TODO - Write the outpu to the file
         try {
             FileWriter myWriter = new FileWriter(FILE_PATH + FILE_NAME + "_results.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            myWriter.write(numberOfPizzaDelivered + "\n");
+
+            for (int i = 0; i < delList.size(); i++) {
+                System.out.println(delList.get(i).toString());
+                myWriter.write(delList.get(i).toString() + "\n");
+            }
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         } catch (IOException e) {
